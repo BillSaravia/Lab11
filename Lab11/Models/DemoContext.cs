e099aced6e286cdad8ecd08f2f9ce616f58f1bd3
BillSarvia <you@example.com>
2023-11-16 21:46:41 -0500

    m

--- Lab11/Models/DemoContext.cs
+++ Lab11/Models/DemoContext.cs
@@ -5,15 +5,15 @@ namespace Lab11.Models
     public class DemoContext : DbContext
     {
 
-        public DemoContext(DbContextOptions<DemoContext> options)
-            : base(options)
-        {
-        }
-
         public DbSet<Course> Course { get; set; }
         public DbSet<Enrollment> Enrollment { get; set; }
         public DbSet<Grade> Grade { get; set; }
         public DbSet<Student> Student { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAB1504-11\\SQLEXPRESS;Database=DemoDB;Trusted_Connection=True;");
+        }
 
     }
 }
